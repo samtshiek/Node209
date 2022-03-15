6a640d1ab67fd3ed44305919ec59923efe9581d7
Your Name <samtshiek@yahoo.fr>
2022-03-15 02:38:28 -0700

    Auction changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -16,8 +16,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     document.getElementById("buttonAdd").addEventListener("click", function () {
 
-        auctionItemArray.push(new auctionItem(document.getElementById("artTitle").value, document.getElementById("artist").value, document.getElementById("artYear").value, document.getElementById("artPrice").value, selectedType));
-        console.log(auctionItemArray);
+        let AuctionObject = new auctionItem(document.getElementById("artTitle").value, document.getElementById("artist").value, document.getElementById("artYear").value, document.getElementById("artPrice").value, selectedType);
+        auctionItemArray.push(AuctionObject);
+
+        $.ajax({
+            url: '/postData',
+            type: 'POST',
+            data: JSON.stringify(AuctionObject),
+            contentType: "application/json; charset=utf-8",
+    
+            success: function (result) {
+                alert(result + " posted");
+    
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("Status: " + textStatus); alert("Error: " + errorThrown);
+            }
+
+            
+        });
+
         document.getElementById("artTitle").value = "";
         document.getElementById("artist").value = "";
         document.getElementById("artYear").value = "";
@@ -49,13 +67,14 @@ function createList() {
     // clear prior data
     $.get('/GetAllData', function(data, status) {
         auctionItemArray = data;
-
+        console.log("Downloaded from server: " + data);
         var myul = document.getElementById("myList");
     myul.innerHTML = '';
 
     auctionItemArray.forEach(function (element,) {   // use handy array forEach method
         var li = document.createElement('li');
         li.classList.add("AuctionList");
+        console.log("ForEach element: " + element);
         li.innerHTML = "$" + element.price + " Art type: " + element.type + " <cite>" + element.title + "</cite>, created by Artist: " + element.artist + " in year " + element.year;
         li.setAttribute("data-param", element.title);
         myul.appendChild(li);
@@ -65,7 +84,7 @@ function createList() {
     var auctionElementArray = Array.from(auctionElements);
     auctionElementArray.forEach(function(element,i) {
         element.addEventListener('click', function() {
-            alert("Click on list # " + (i+1));
+            alert("Clicked on list # " + (i+1));
             var param = this.getAttribute("data-param");
             localStorage.setItem("param", param);
             document.location.href = "index.html#details";
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-const ServerAuctionItems = [
+let ServerAuctionItems = [
   {title: 'Philosopher in meditation', artist: 'Rembrandt', year: '1632', price: 11450000, type: 'Painting' },
   {title: 'Guennol Lioness', artist: 'unknown', year: '5000 B.C.', price: 57000000, type: 'Sculpture' },
   {title: 'La Terrace Ã Sainte-Adresse', artist: 'Claude Monet', year: '1867', price: 11500, type: 'Painting' }
 ]
 
-console.log(ServerAuctionItems);
+var fs = require('fs');
+
+let fileManager = {
+  read: function() {
+    const stat = fs.statSync('auction-data.json');
+    if (stat.size !== 0) {
+      var rawdata = fs.readFileSync('auction-data.json');
+    ServerAuctionItems = JSON.parse(rawdata);
+    }
+    else {
+      ServerAuctionItems = [
+        {title: 'Philosopher in meditation', artist: 'Rembrandt', year: '1632', price: 11450000, type: 'Painting' },
+        {title: 'Guennol Lioness', artist: 'unknown', year: '5000 B.C.', price: 57000000, type: 'Sculpture' },
+        {title: 'La Terrace Ã Sainte-Adresse', artist: 'Claude Monet', year: '1867', price: 11500, type: 'Painting' }
+      ]
+      fileManager.write();
+    }
+  },
+
+  write: function() {
+    let data = JSON.stringify(ServerAuctionItems);
+    fs.writeFileSync('auction-data.json', data);
+  }
+}
+
+
+console.log("From server: " + ServerAuctionItems);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,9 +42,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/GetAllData', function(req, res) {
+  fileManager.read();
   res.status(200).json(ServerAuctionItems);
 });
 
+router.post('/postData', function(req, res) {
+  console.log("Request body: " + req.body);
+  ServerAuctionItems.push(req.body);
+  fileManager.write();
+  console.log("Array after post: " + ServerAuctionItems);
+})
 
 
 module.exports = router;
